8f38bc27f90a552ff8d5df561d8861f2a24e6506
gfernandes728 <guga.728@gmail.com>
2025-07-11 12:01:39 -0300

    Melhoria js

---
+++ GustavoDocSpiderTeste/wwwroot/js/layout.js
@@ -0,0 +1,34 @@
+﻿var layout = {
+    init: function() {
+
+    },
+    toggleSidebar: function () {
+        const el = $(".title-menu");
+        const overlay = $(".overlay");
+
+        this.toggleList(el, "d-none");
+
+        $("#sidebar")
+            .toggleClass("side-bar-close")
+            .toggleClass("mobile-visible");
+
+        this.toggleList(overlay, "show");
+    },
+    toggleList: function (el, cl) {
+        const total = el.length;
+
+        for (let i = 0; i < total; i++) {
+            $(el[i]).toggleClass(cl);
+        }
+    },
+    closeSidebar: function () {
+        $("#sidebar").removeClass("mobile-visible");
+
+        const overlay = $(".overlay");
+        const total = overlay.length;
+
+        for (let i = 0; i < total; i++) {
+            $(overlay[i]).removeClass("show");
+        }
+    }
+};
